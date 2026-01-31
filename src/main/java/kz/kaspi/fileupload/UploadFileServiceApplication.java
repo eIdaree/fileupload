66c4322f4b8bbb9fe2798c1e59628c61f334c2ad
eldar <eldaryergengali@gmail.com>
2026-01-31 14:43:59 +0500

    init: project init

---
+++ src/main/java/kz/kaspi/fileupload/UploadFileServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.kaspi.fileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadFileServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadFileServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/kaspi/fileupload/UploadFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.kaspi.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadFileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
